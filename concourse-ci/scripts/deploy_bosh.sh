df5d81eca7a6e2f6a3bc4a4be09f12b3f89eebb9
norshtein <t-tosi@microsoft.com>
2018-06-14 08:35:54 +0000

    Add deploy bosh

--- concourse-ci/scripts/deploy_bosh.sh
+++ concourse-ci/scripts/deploy_bosh.sh
@@ -2,13 +2,20 @@
 
 set -e
 
-export BOSH_INIT_LOG_LEVEL="Debug"
-export BOSH_INIT_LOG_PATH="./run.log"
-bosh-init deploy ~/bosh.yml
+export BOSH_LOG_LEVEL="Debug"
+export BOSH_LOG_PATH="./run.log"
 
-bosh target REPLACE_WITH_BOSH_DIRECTOR_IP >/dev/null 2>&1 << EndOfMessage
-admin
-REPLACE_WITH_ADMIN_PASSWORD
-EndOfMessage
-
-sed -i -e "s/REPLACE_WITH_DIRECTOR_ID/$(bosh status --uuid)/" ./concourse.yml
+bosh create-env ~/example_manifests/bosh.yml \
+	--state=state.json \
+	--vars-store=~/bosh-deployment-vars.yml \
+	-o ~/example_manifests/cpi.yml \
+	-o ~/example_manifests/custom-cpi-release.yml \
+	-o ~/example_manifests/custom-environment.yml \
+	-o ~/example_manifests/use-azure-dns.yml \
+	-o ~/example_manifests/jumpbox-user.yml \
+	-o ~/example_manifests/keep-failed-or-unreachable-vms.yml \
+	-v director_name=azure \
+	-v internal-cidr=10.0.0.0/24 \
+	-v internal_gw=10.0.0.1 \
+	-v internal_ip=10.0.0.4 \
+	-v cpi_release_url=
